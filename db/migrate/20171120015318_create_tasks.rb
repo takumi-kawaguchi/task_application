8c785f101cea837ff16a6084f6b65166926a313b
川口拓巳 <kawaguchitakumi@kawaguchitakuminoMacBook-Pro.local>
2017-11-20 10:58:21 +0900

    Taskモデル作成

---
+++ db/migrate/20171120015318_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :description
+      t.integer :priority
+      t.integer :status
+      t.date :finish_at
+
+      t.timestamps
+    end
+  end
+end
