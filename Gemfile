47258e8f8d3a58b4da514afdef9e99ac9f776c8d
川口拓巳 <kawaguchitakumi@kawaguchitakuminoMacBook-Pro.local>
2017-11-21 11:21:25 +0900

    カラムの修正、jquery導入

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
+gem "jquery-rails"
 
 gem 'haml-rails'
 gem 'erb2haml'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120015318) do
+ActiveRecord::Schema.define(version: 20171121020410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20171120015318) do
     t.string "title"
     t.string "description"
     t.integer "priority"
-    t.integer "status"
+    t.string "status"
     t.date "finish_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
